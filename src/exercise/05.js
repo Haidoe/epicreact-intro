642b9be34d084cbc39af71d95ef523bcfa509e10
haidoe <haidren.amalia@gmail.com>
2022-11-11 15:11:23 -0800

    Clean up

--- src/exercise/05.js
+++ src/exercise/05.js
@@ -4,16 +4,6 @@
 import * as React from 'react'
 import '../box-styles.css'
 
-// 🐨 add a className prop to each div and apply the correct class names
-// based on the text content
-// 💰 Here are the available class names: box, box--large, box--medium, box--small
-// 💰 each of the elements should have the "box" className applied
-
-// 🐨 add a style prop to each div so their background color
-// matches what the text says it should be
-// 🐨 also use the style prop to make the font italic
-// 💰 Here are available style attributes: backgroundColor, fontStyle
-
 const Box = props => {
   const {children, className, style, size, ...others} = props
   const subClass = size ? `box--${size}` : ''
