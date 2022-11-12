53cd96667429d04d2067a60c552967600b83f1a7
haidoe <haidren.amalia@gmail.com>
2022-11-11 17:37:16 -0800

    Exercise 6 - Credit 3, useState with the username input

--- src/exercise/06.js
+++ src/exercise/06.js
@@ -4,42 +4,18 @@
 import * as React from 'react'
 
 function UsernameForm({onSubmitUsername}) {
-  // 🐨 add a submit event handler here (`handleSubmit`).
-  // 💰 Make sure to accept the `event` as an argument and call
-  // `event.preventDefault()` to prevent the default behavior of form submit
-  // events (which refreshes the page).
-  // 📜 https://developer.mozilla.org/en-US/docs/Web/API/Event/preventDefault
-  //
-  // 🐨 get the value from the username input (using whichever method
-  // you prefer from the options mentioned in the instructions)
-  // 💰 For example: event.target.elements[0].value
-  // 🐨 Call `onSubmitUsername` with the value of the input
-
-  // 🐨 add the onSubmit handler to the <form> below
-
-  // 🐨 make sure to associate the label to the input.
-  // to do so, set the value of 'htmlFor' prop of the label to the id of input
-  const inputRef = React.useRef(null)
-  const [errorMsg, setErrorMsg] = React.useState(null)
+  const [username, setUsername] = React.useState('')
 
   const handleSubmit = evt => {
     evt.preventDefault()
 
-    onSubmitUsername(inputRef.current.value)
+    onSubmitUsername(username)
   }
 
   const handlelChange = evt => {
     const value = evt.target.value
-    const isValid = value === value.toLowerCase()
-
-    setErrorMsg(isValid ? null : 'Username must be lower case')
+    setUsername(value.toLowerCase())
   }
-
-  const Alert = () => (
-    <p role="alert" style={{color: 'red'}}>
-      {errorMsg}
-    </p>
-  )
 
   return (
     <form onSubmit={handleSubmit}>
@@ -49,16 +25,12 @@ function UsernameForm({onSubmitUsername}) {
         <input
           id="username"
           type="text"
-          ref={inputRef}
+          value={username}
           onChange={handlelChange}
         />
-
-        {Boolean(errorMsg) && <Alert />}
       </div>
 
-      <button type="submit" disabled={Boolean(errorMsg)}>
-        Submit
-      </button>
+      <button type="submit">Submit</button>
     </form>
   )
 }
